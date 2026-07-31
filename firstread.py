5ec1e29a136853db44513df28db1db52e83e8c84
Amar Deno Tahirovic <amartahirovic524@gmail.com>
2026-07-31 17:53:27 -0500

    P message field extraction: symbol, shares, price verified against known July 2019 closes

--- firstread.py
+++ firstread.py
@@ -1,6 +1,6 @@
 Itch = open("07302019.NASDAQ_ITCH50", "rb")
-counts = {}
-n = 0
+counts = {} 
+n = 0 # We say n = 0 to make n exist for the later comparison
 while True:
     prefix = Itch.read(2)
     if len(prefix) < 2: 
@@ -10,6 +10,11 @@ while True:
     if length == 0 or len(message) < length:
         break 
     letter = chr(message[0])
+    if letter == "P":
+        shares = int.from_bytes(message[20:24], "big")
+        stock = message[24:32].decode("ascii").rstrip() 
+        price = int.from_bytes(message[32:36], "big")
+        print(stock, shares, price)
     counts[letter] = counts.get(letter,0) + 1 
     n = n + 1
     if n >= 1000000:
