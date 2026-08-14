406a991388ea084116715f0fda29d6a1023bf687
Amar.Tahirovic <amartahirovic524@gmail.com>
2026-08-14 15:57:08 -0500

    Green Test compete. Expecations met, Sized literals saved me a bug where count <= count + 1; + 1 caught a width error during compilation. Definitely keep using them. Bit values & Bit positions. Output logic and count had same bit value, literal bit value was ok at 4, output logic should've been 3 considering it goes 4, 3, 2, 1, 0. 5 bits.

--- rtl/counter.sv
+++ rtl/counter.sv
@@ -1,11 +1,13 @@
 module counter (
     input logic clk,
     input logic rst_n,
-    output logic [4:0]count
+    output logic [3:0]count
 );
 
     always_ff @(posedge clk) begin
         if (!rst_n)
             count <= 4'd0;
-    else  
+    else 
+        count <= count + 1;
         end
+    endmodule 
